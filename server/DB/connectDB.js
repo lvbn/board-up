200f5f46314af8888312aa578b8c2fa9a12bd038
Federico <federico.b.giordani@gmail.com>
2023-04-28 18:45:57 +0200

    removed folders

--- server/DB/connectDB.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-async function connectDB () {
-  await mongoose.connect('mongodb://127.0.0.1:27017/boardUp', { useNewUrlParser: true });
-  console.log('Connected to DB 🐰')
-};
-
-
-module.exports = connectDB;
