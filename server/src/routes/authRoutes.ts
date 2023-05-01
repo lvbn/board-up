b67b5ae96314949f1611e0073057f6e8b08d01c7
Federico <federico.b.giordani@gmail.com>
2023-05-01 18:18:30 +0200

    patch: changed auth routes

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { signin } from '../controllers/authController';
 
 const router = Router();
-ƒ
+
 router.get('/', signin);
 
 export { router as authRouter };
